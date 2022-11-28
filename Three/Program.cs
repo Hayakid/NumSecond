50bdf548a6b4be131ff62dfad57737721a5e1ab6
Danil <hisdan56708@gmail.com>
2022-11-29 02:46:31 +0300

    three hours of night

---
+++ Three/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// class program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Input how much");
+//         string N = Console.ReadLine();
+//         {
+//             for (int i = 1; i <= N; i++)
+//             {
+//                 i=i*i*i;
+//                 Console.WriteLine(i);
+               
+//             }
+
+//         }
+//     }
+// }
+
+
+void Sqrt1(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        Console.WriteLine($"{i}-->{i*i*i}");
+    }
+}
+Console.WriteLine("Input N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Sqrt1(N);
