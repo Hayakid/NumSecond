408572aa13ab51af860f2856a32b4e253e872044
Danil <hisdan56708@gmail.com>
2022-12-01 01:27:42 +0300

    today in 1;30

---
+++ Firs/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 2.Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+
+  Console.Write("input number A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("input number B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Отве " + exponentiation);
--- Numbertwo/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿double GetLens(int x1,int y1,int z1, int x2,int y2,int z2)
-{
-double len = Math.Sqrt((x1-x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
-return len;
-}
-Console.WriteLine(GetLens(3, 6, 8, 2, 1, -7));    
